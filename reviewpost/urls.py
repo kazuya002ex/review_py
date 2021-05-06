6b81275c32a88b8e322d4a5a8766171260aa1020
kazuya002ex <chiharu.event2@gmail.com>
2021-05-06 16:19:34 +0900

    feat: loginまでじっおう

--- reviewpost/urls.py
+++ reviewpost/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import signupview
+from .views import signupview, loginview
 
 urlpatterns = [
   path('admin/', admin.site.urls),
   path('signup/', signupview, name='signup'),
+  path('login/', loginview, name='login'),
 ]
