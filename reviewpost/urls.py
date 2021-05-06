4d946010ebb877dc6257b9357d4f7152a0abb5e0
kazuya002ex <chiharu.event2@gmail.com>
2021-05-06 16:47:13 +0900

    feat: listページの作成

--- reviewpost/urls.py
+++ reviewpost/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import signupview, loginview
+from .views import signupview, loginview, listview
 
 urlpatterns = [
   path('admin/', admin.site.urls),
   path('signup/', signupview, name='signup'),
   path('login/', loginview, name='login'),
+  path('list/', listview, name='list'),
 ]
